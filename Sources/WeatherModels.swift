6199ca530d471735a5ce943f575853f041403f3d
Florentin Portets <florentin.portets@ynov.com>
2026-02-26 14:08:00 +0100

    Implement weather data models for JSON decoding

--- Sources/WeatherModels.swift
+++ Sources/WeatherModels.swift
@@ -7,23 +7,37 @@ import Foundation
 // Décommenter et compléter les structures pour le décodage JSON
 
 // TODO 1.1: CurrentWeather avec temperature, windspeed, weathercode
-// struct CurrentWeather: Decodable {
-//     // ...
-// }
+struct CurrentWeather: Decodable {
+    let temperature: Double
+    let windspeed: Double
+    let weathercode: Int
+}
 
 // TODO 1.2: WeatherResponse avec latitude, longitude, currentWeather
 // Astuce: "current_weather" dans le JSON -> utiliser CodingKeys
-// struct WeatherResponse: Decodable {
-//     // ...
-// }
+struct WeatherResponse: Decodable {
+    let latitude: Double
+    let longitude: Double
+    let currentWeather: CurrentWeather
+    
+    enum CodingKeys: String, CodingKey {
+        case latitude = "latitude"
+        case longitude = "longitude"
+        case currentWeather = "current_weather"
+    }
+}
 
 // TODO 1.3: City avec name, latitude, longitude
-// struct City {
-//     // ...
-// }
+struct City {
+    let name: String
+    let latitude: Double
+    let longitude: Double
+}
 
 // 2. ERRORS (1 pt)
 // TODO 2.1: WeatherError avec cas invalidURL, networkError(String), decodingError(String)
-// enum WeatherError: Error {
-//     // ...
-// }
+enum WeatherError: Error {
+    case invalidURL
+    case networkError(String)
+    case decodingError(String)
+}
